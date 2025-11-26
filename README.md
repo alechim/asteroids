ac9b717ffdcc23075b92c5d45eeb55b8bdc64df6
alechim <alechim74@gmail.com>
2025-11-26 10:34:47 -0800

    Updated README and implemented game movement

---
+++ README.md
@@ -0,0 +1,11 @@
+# asteroids
+
+This is my second boot.dev project. This was programmed in Python and uses the PyGame library.
+
+## About
+
+## Usage
+
+---
+
+*Use `python3` instead of `python` on Unix-based systems (Linux/macOS/WSL) if needed.*
--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
+    print("DEBUG: entered main()")
+
     # Initializes pygame
     pygame.init()
+    gameClock = pygame.time.Clock()
+    dt = 0      # Delta Time
+
+    # Create two groups for player
+    # - Updateable: hold all objects that can be updated
+    # - Drawable: hold all objects that can be drawn
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    # Generate the player
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    # Explicitly add player instanced object into the groups
+    updatable.add(player)
+    drawable.add(player)
 
     # Get a new instance of GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Game Loop
     while True:
         # Logs the games frames once per second
         log_state()
@@ -21,8 +43,17 @@ def main():
         # Fills the screen with a solid black color
         screen.fill("black")
 
+        # Use the groups to update and render player & objects
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         # Refreshes the screen
         pygame.display.flip()
+
+        # Pause the game loop until 1/60th of a second has passed and holds the amount of time passed since last frame
+        dt = gameClock.tick(60) / 1000
 
     # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     # print(f"Screen width: {SCREEN_WIDTH}")
